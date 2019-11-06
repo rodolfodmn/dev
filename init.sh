36532c92d572dd0a9a89821f4e75e342e40f891d
Rodolfo Domingos <rodolfo.domingos@bistwobis.com.br>
2019-11-06 13:59:47 -0300

    inicio fazendo os clones

---
+++ init.sh
@@ -0,0 +1,7 @@
+echo 'cirando ambiente de desenvolvimento maroto :D'
+mkdir dbs
+git clone https://github.com/rodolfodmn/dotfiles
+git clone https://github.com/rodolfodmn/php_bash.git
+git clone https://github.com/rodolfodmn/bash
+git clone https://github.com/rodolfodmn/magentoTinyFixes
+
