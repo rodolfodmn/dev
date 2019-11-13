4e0b85921f8dac69d4f2fa9d042bec708805c1a0
Rodolfo Domingos <rodolfo.dmn@gmail.com>
2019-11-13 10:47:23 -0300

    clonando vundle

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ git clone https://github.com/rodolfodmn/dotfiles
 git clone https://github.com/rodolfodmn/php_bash.git
 git clone https://github.com/rodolfodmn/bash
 git clone https://github.com/rodolfodmn/magentoTinyFixes
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp ./dotfiles/.tmux.conf ~/
 cp ./dotfiles/.zshrc ~/
