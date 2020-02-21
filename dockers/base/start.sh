c7d675814a89d9eebef1d7a056fdd5e152eb0cd2
Rodolfo Domingos <rodolfo.domingos@bistwobis.com.br>
2020-02-21 09:28:40 -0300

    melhorias no install da loja

--- dockers/base/start.sh
+++ dockers/base/start.sh
@@ -4,7 +4,7 @@ apt install -y mysql-server-5.6
 add-apt-repository -y ppa:ondrej/php
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
 apt update
-apt install -y php5.6 php5.6-json php5.6-curl php5.6-xml php5.6-mysql php5.6-mcrypt php5.6-zip
+apt install -y php5.6 php5.6-json php5.6-curl php5.6-xml php5.6-mysql php5.6-mcrypt php5.6-zip php5.6-soap
 apt install -y libapache2-mod-php5.6
 nvim /etc/apache2/apache2.conf
 nvim /etc/apache2/sites-available/default-ssl.conf 
