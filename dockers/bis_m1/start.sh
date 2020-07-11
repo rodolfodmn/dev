92cbc5a8cfe22d6bd613b6d2d70061ad389efcc7
Rodolfo Domingos <rodolfo.dmn@gmail.com>
2020-07-10 22:26:29 -0300

    modificando ideia dos dockers

--- dockers/bis_m1/start.sh
+++ dockers/bis_m1/start.sh
@@ -10,10 +10,6 @@ apt install -y \
     php5.6-gd php5.6-json php5.6-curl php5.6-xml php5.6-mysql php5.6-mcrypt php5.6-zip php5.6-soap php5.6-mbstring php5.6-xdebug php5.6-intl
 
 apt install -y libapache2-mod-php5.6
-# cd ~
-# wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
-# ./nvim.appimage --appimage-extract
-# cp ~/squashfs-root/usr/* /usr -r
 
 #Install node 12 <
 curl -sL https://deb.nodesource.com/setup_12.x -o ~/nodesource_setup.sh
