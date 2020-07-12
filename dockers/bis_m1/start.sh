19a36780a2ffed8cfcda773e9afea80e2a1ff8ed
Rodolfo Domingos <rodolfo.dmn@gmail.com>
2020-07-11 21:56:04 -0300

    add validacao para primeriro inicio

--- dockers/bis_m1/start.sh
+++ dockers/bis_m1/start.sh
@@ -1,30 +1,13 @@
-echo "hello rods ;D"
-passwd rods
-gem install compass
-apt install -y mysql-server-5.6
-add-apt-repository -y ppa:ondrej/php
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4F4EA0AAE5267A6C
-apt update
-apt install -y \
-    php5.6 \
-    php5.6-gd php5.6-json php5.6-curl php5.6-xml php5.6-mysql php5.6-mcrypt php5.6-zip php5.6-soap php5.6-mbstring php5.6-xdebug php5.6-intl
+#!/bin/bash
 
-apt install -y libapache2-mod-php5.6
+if [ -z "$1" ]; then
+    if [ "$1" == 'new' ]; then
+        echo "iniciando uma nova imagem!";
+        passwd rods
+        apt install -y mysql-server-5.6
 
-#Install node 12 <
-curl -sL https://deb.nodesource.com/setup_12.x -o ~/nodesource_setup.sh
-bash nodesource_setup.sh
-apt install nodejs
-#Install node 12 >
-
-#npm user config <
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-#npm user config >
-
-vim /etc/apache2/apache2.conf
-vim /etc/apache2/sites-available/default-ssl.conf 
-vim /etc/apache2/sites-available/000-default.conf
-mkdir -p /var/www/html/stores
-chmod 777 /var/www/html -R
-sudo service apache2 restart
+        vim /etc/apache2/apache2.conf
+        vim /etc/apache2/sites-available/default-ssl.conf 
+        vim /etc/apache2/sites-available/000-default.conf
+    fi
+fi
