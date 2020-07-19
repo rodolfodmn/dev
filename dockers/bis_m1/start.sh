1e6ccd0ee1f5bd77939ecdbd13ec63b2885182c3
Rodolfo Domingos <rodolfo.dmn@gmail.com>
2020-07-19 00:17:43 -0300

    melhorias dockerfile bism1

--- dockers/bis_m1/start.sh
+++ dockers/bis_m1/start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    if [ "$1" == 'new' ]; then
-        echo "iniciando uma nova imagem!";
-        passwd rods
-        apt install -y mysql-server-5.6
+echo "iniciando uma nova imagem!";
+sudo apt install -y mysql-server-5.6
 
-        vim /etc/apache2/apache2.conf
-        vim /etc/apache2/sites-available/default-ssl.conf 
-        vim /etc/apache2/sites-available/000-default.conf
-    fi
-fi
+sudo vim /etc/apache2/apache2.conf
+sudo vim /etc/apache2/sites-available/default-ssl.conf 
+sudo vim /etc/apache2/sites-available/000-default.conf
+#if [ -z "$1" ]; then
+	#if [ "$1" == 'new' ]; then
+	#fi
+#fi
